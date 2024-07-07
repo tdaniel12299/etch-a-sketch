c44ca290fbd5e4dffabc033c406998ba14ffecaa
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-07-07 03:05:07 -0400

    refactored the javascript by implementing grid-box styles into the desired size

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
-let rowsWidth = 5;
-let columnsHeight = 5;
-let numOfDiv = 25;
+// let rowsWidth = 25;
+// let columnsHeight = 25;
+// let numOfDiv = 25;
 
 
+// let createGrid = (rowsWidth, columnsHeight) => {
+//     const gridContainer = document.getElementById('grid');
+//     // gridContainer.style.display = 'grid';
+//     // gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 1fr)`;
+//     // gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 1fr)`;
 
-let createGrid = (rowsWidth, columnsHeight) => {
-    const gridContainer = document.getElementById('grid');
-    // gridContainer.style.display = 'grid';
-    // gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 1fr)`;
-    // gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 1fr)`;
+//     for (let i = 0; i < rowsWidth * columnsHeight; i++ ) {
+//         const gridItem = document.createElement('div');
+//         gridItem.className = 'item';
+//         gridItem.textContent = "w";
+//         gridItem.style.border = "2px solid black";
 
-    for (let i = 0; i < rowsWidth * columnsHeight; i++ ) {
-        const gridItem = document.createElement('div');
-        gridItem.className = 'item';
-        gridItem.textContent = "w";
-        gridItem.style.border = "2px solid black";
+//         gridContainer.style.display = 'grid';
+//         gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 5fr)`;
+//         gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 5fr)`;
+        
 
-        gridContainer.style.display = 'grid';
-        gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 5fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 1fr)`;
+//         gridContainer.appendChild(gridItem)
+//     }
 
-        gridContainer.appendChild(gridItem)
-    }
+// }
 
+// createGrid(rowsWidth,columnsHeight)
+
+
+let numOfDivs = 5;
+let width = 5
+let lenght = 5
+
+let widthAndHeight = () => {
+    return width/numOfDivs
 }
 
-createGrid(rowsWidth,columnsHeight)
+let createGrid = () => {
+    gridContainer = document.getElementById('grid');
+    gridContainer.style.display = 'grid';
+    gridContainer.style.gridTemplateColumns = `repeat(${lenght}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${width}, 1fr)`
 
+    
+    
+    for (let i=0; i < numOfDivs**2; i++) {
+        const item = document.createElement("div");
+    
+        item.className = 'item';
+        item.textContent = "";
+        item.style.border = "2px solid black";
 
+        gridContainer.appendChild(item);
+    }
+}
+
+createGrid()
