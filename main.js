89437272b3b257b5bdcd5ead2b9f52616e7a1603
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-07-07 03:20:58 -0400

    refactored createGrid function to test ability to change grid size and removed old code

--- main.js
+++ main.js
@@ -1,54 +1,18 @@
-// let rowsWidth = 25;
-// let columnsHeight = 25;
-// let numOfDiv = 25;
 
-
-// let createGrid = (rowsWidth, columnsHeight) => {
-//     const gridContainer = document.getElementById('grid');
-//     // gridContainer.style.display = 'grid';
-//     // gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 1fr)`;
-//     // gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 1fr)`;
-
-//     for (let i = 0; i < rowsWidth * columnsHeight; i++ ) {
-//         const gridItem = document.createElement('div');
-//         gridItem.className = 'item';
-//         gridItem.textContent = "w";
-//         gridItem.style.border = "2px solid black";
-
-//         gridContainer.style.display = 'grid';
-//         gridContainer.style.gridTemplateColumns = `repeat(${columnsHeight}, 5fr)`;
-//         gridContainer.style.gridTemplateRows = `repeat(${rowsWidth}, 5fr)`;
-        
-
-//         gridContainer.appendChild(gridItem)
-//     }
-
-// }
-
-// createGrid(rowsWidth,columnsHeight)
-
-
-let numOfDivs = 5;
-let width = 5
-let lenght = 5
-
-let widthAndHeight = () => {
-    return width/numOfDivs
-}
+let numOfDivs = 10;
 
 let createGrid = () => {
     gridContainer = document.getElementById('grid');
     gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateColumns = `repeat(${lenght}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${width}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${numOfDivs}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${numOfDivs}, 1fr)`
 
-    
+
     
     for (let i=0; i < numOfDivs**2; i++) {
         const item = document.createElement("div");
     
         item.className = 'item';
-        item.textContent = "";
         item.style.border = "2px solid black";
 
         gridContainer.appendChild(item);
