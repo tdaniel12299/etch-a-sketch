d2120917752548e003d80929e12f041bdd9e7371
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-07-08 22:30:41 -0400

    styles added to gridContainer and header

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-let defaultNum = 5;
 let slider = document.getElementById("range1")
 let output = document.getElementById("value")
 gridContainer = document.getElementById('grid');
@@ -9,6 +8,8 @@ output.innerHTML = slider.value;
 
 let createGrid = (defaultNum) => {
 
+// while loop is to clear the container so the new divs do stack over old divs, used for resizing container
+
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild)
     }
@@ -17,7 +18,7 @@ let createGrid = (defaultNum) => {
     gridContainer.style.gridTemplateColumns = `repeat(${defaultNum}, 1fr)`
     gridContainer.style.gridTemplateRows = `repeat(${defaultNum}, 1fr)`
 
-
+// for loop creates grid 
     
     for (let i=0; i < defaultNum**2; i++) {
         const item = document.createElement("div");
@@ -32,19 +33,23 @@ let createGrid = (defaultNum) => {
 document.addEventListener('DOMContentLoaded',() => {
     gridContainer = document.getElementById('grid')
 })
-slider.oninput = ()=>{
-    
+
+// connects the slider functionalty to the displayed value and number of divs
+
+slider.oninput = ()=>{    
     output.innerHTML = slider.value;
     createGrid(slider.value)
-    
-    
 }
+
+// adds stylization to slider for a smoother transition 
 
 slider.addEventListener("mousemove", ()=>{
     let x = slider.value;
     let color = 'linear-gradient(90deg, rgb(118, 118, 233)' + x + '%, rgb(193, 193, 238)' + x +'%';
     slider.style.background = color;
 })
+
+
 
 createGrid(slider.value)
 
