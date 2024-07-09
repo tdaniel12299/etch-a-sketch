839fc978e58eb79873f752c825650ae25457cdd3
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-07-08 21:29:10 -0400

    refactored js to incoporate slider-div resizing functionalty

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 
-let numOfDivs = 5;
+let defaultNum = 5;
 let slider = document.getElementById("range1")
 let output = document.getElementById("value")
+gridContainer = document.getElementById('grid');
+
 output.innerHTML = slider.value;
 
 
-let createGrid = () => {
-    gridContainer = document.getElementById('grid');
+let createGrid = (defaultNum) => {
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+
     gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateColumns = `repeat(${numOfDivs}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${numOfDivs}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${defaultNum}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${defaultNum}, 1fr)`
 
 
     
-    for (let i=0; i < numOfDivs**2; i++) {
+    for (let i=0; i < defaultNum**2; i++) {
         const item = document.createElement("div");
     
         item.className = 'item';
@@ -23,17 +29,24 @@ let createGrid = () => {
         gridContainer.appendChild(item);
     }
 }
-
-
+document.addEventListener('DOMContentLoaded',() => {
+    gridContainer = document.getElementById('grid')
+})
 slider.oninput = ()=>{
-    output.innerHTML = slider.value
+    
+    output.innerHTML = slider.value;
+    createGrid(slider.value)
+    
+    
 }
+
 slider.addEventListener("mousemove", ()=>{
     let x = slider.value;
     let color = 'linear-gradient(90deg, rgb(118, 118, 233)' + x + '%, rgb(193, 193, 238)' + x +'%';
     slider.style.background = color;
 })
 
+createGrid(slider.value)
 
 
-createGrid()
+
